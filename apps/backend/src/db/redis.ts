f2eca12080fa5e0659ac712135168b1b4b7f3ddb
Sayan Ghosh <sayanghosh97x@gmail.com>
2025-06-05 17:12:58 +0530

    Implement redis client and limit recent runlogs sorted set to 10 entries

--- apps/backend/src/db/redis.ts
+++ apps/backend/src/db/redis.ts
@@ -1,1 +1,34 @@
 // redis.ts
+import { createClient } from 'redis';
+import { RunLog } from '../utils/types';
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
+});
+
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+await redisClient.connect();
+
+export async function cacheRun(log: RunLog): Promise<void> {
+  try {
+    const key = `runlog:${log.timestamp!.toISOString()}`;
+    await redisClient.set(key, JSON.stringify(log), {
+      EX: 3600, // expire in 1 hour
+    });
+
+    // Add the key to a sorted set with timestamp as score
+    await redisClient.zAdd('recent_runlogs', {
+      score: log.timestamp!.getTime(),
+      value: key,
+    });
+
+  
+    const totalCount = await redisClient.zCard('recent_runlogs');
+    if (totalCount > 10) {
+      await redisClient.zRemRangeByRank('recent_runlogs', 0, totalCount - 11); // It will store upto 10 user requests
+    }
+  } catch (err) {
+    console.error('Failed to cache run log in Redis:', err);
+  }
+}
